76995021c3f809462624d0beb1639b4b583f0b87
tiwarivaibhav0 <tiwarivaibhav0@gmail.com>
2024-01-31 21:56:05 +0530

    nested structure of created with react createElement

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const heading = React.createElement("h1", {id:"heading"}, "Helo from reactJS");
+const heading = React.createElement("div", { id: "parent" }, [
+	React.createElement("div", { id: "child1" }, [
+		React.createElement("h1", { id: "" }, "I am an h1"),
+		React.createElement("h2", { id: "" }, "I am an h2"),
+	]),
+	React.createElement("div", { id: "child2" }, [
+		React.createElement("h1", { id: "" }, "I am an h1"),
+		React.createElement("h2", { id: "" }, "I am an h2"),
+	]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
